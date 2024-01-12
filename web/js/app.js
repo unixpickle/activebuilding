4ae79aa72cec95186017167b32fbbdf002fb1776
Alex Nichol <unixpickle@gmail.com>
2024-01-11 21:19:37 -0800

    loaders; populate panels

--- web/js/app.js
+++ web/js/app.js
@@ -32,6 +32,7 @@ class Panel {
             }
             this.listElement.innerHTML = '';
             results.forEach((x) => this.listElement.appendChild(this.createListItem(x)));
+            this.container.classList.remove('panel-loading');
         });
     }
     showError(e) {
@@ -54,14 +55,14 @@ class MessagesPanel extends Panel {
         sender.className = 'message-item-username';
         sender.textContent = item.username;
         result.appendChild(sender);
-        const subject = document.createElement('label');
-        subject.className = 'message-item-subject';
-        subject.textContent = item.subject;
-        result.appendChild(subject);
         const date = document.createElement('label');
         date.className = 'message-item-date';
         date.textContent = item.last_activity;
         result.appendChild(date);
+        const subject = document.createElement('label');
+        subject.className = 'message-item-subject';
+        subject.textContent = item.subject;
+        result.appendChild(subject);
         return result;
     }
 }
@@ -77,6 +78,21 @@ class WallPanel extends Panel {
     createListItem(item) {
         const result = document.createElement('li');
         result.className = 'wall-post-item';
+        const header = document.createElement('div');
+        header.className = 'wall-post-item-header';
+        const posterName = document.createElement('label');
+        posterName.className = 'wall-post-item-poster-name';
+        posterName.textContent = item.poster_name;
+        const postTime = document.createElement('label');
+        postTime.className = 'wall-post-item-post-time';
+        postTime.textContent = item.relative_time;
+        header.appendChild(posterName);
+        header.appendChild(postTime);
+        result.appendChild(header);
+        const contents = document.createElement('div');
+        contents.className = 'wall-post-item-contents';
+        contents.innerHTML = item.contents_html;
+        result.appendChild(contents);
         return result;
     }
 }
@@ -92,7 +108,39 @@ class PackagesPanel extends Panel {
     createListItem(item) {
         const result = document.createElement('li');
         result.className = 'package-item';
+        const fields = document.createElement('div');
+        fields.className = 'package-item-fields';
+        fields.appendChild(this.createField('Type', item.type));
+        fields.appendChild(this.createField('Arrival', item.arrival));
+        if (item.description) {
+            fields.appendChild(this.createField('Description', item.description));
+        }
+        fields.appendChild(this.createField('Accepted By', item.accepted_by));
+        fields.appendChild(this.createField('Released By', item.released_by));
+        result.appendChild(fields);
+        const icon = document.createElement('img');
+        icon.className = 'package-item-icon';
+        if (item.type == 'USPS') {
+            icon.src = "/svg/usps.svg";
+        }
+        else {
+            icon.src = "/svg/amazon_box.svg";
+        }
+        result.appendChild(icon);
         return result;
+    }
+    createField(name, value) {
+        const field = document.createElement('div');
+        field.className = 'package-item-field';
+        const nameLabel = document.createElement('label');
+        nameLabel.className = 'package-item-field-name';
+        nameLabel.textContent = name;
+        const valueLabel = document.createElement('label');
+        valueLabel.className = 'package-item-field-value';
+        valueLabel.textContent = value;
+        field.appendChild(nameLabel);
+        field.appendChild(valueLabel);
+        return field;
     }
 }
 window.app = new App();
--- web/ts/app.ts
+++ web/ts/app.ts
@@ -36,6 +36,7 @@ abstract class Panel<Item> {
         }
         this.listElement.innerHTML = '';
         results.forEach((x) => this.listElement.appendChild(this.createListItem(x)));
+        this.container.classList.remove('panel-loading');
     }
 
     showError(e: string) {
@@ -60,14 +61,14 @@ class MessagesPanel extends Panel<MessageListing> {
         sender.className = 'message-item-username';
         sender.textContent = item.username;
         result.appendChild(sender);
-        const subject = document.createElement('label');
-        subject.className = 'message-item-subject';
-        subject.textContent = item.subject;
-        result.appendChild(subject);
         const date = document.createElement('label');
         date.className = 'message-item-date';
         date.textContent = item.last_activity;
         result.appendChild(date);
+        const subject = document.createElement('label');
+        subject.className = 'message-item-subject';
+        subject.textContent = item.subject;
+        result.appendChild(subject);
         return result;
     }
 }
@@ -84,6 +85,21 @@ class WallPanel extends Panel<WallPost> {
     createListItem(item: WallPost): HTMLLIElement {
         const result = document.createElement('li');
         result.className = 'wall-post-item';
+        const header = document.createElement('div');
+        header.className = 'wall-post-item-header';
+        const posterName = document.createElement('label');
+        posterName.className = 'wall-post-item-poster-name';
+        posterName.textContent = item.poster_name;
+        const postTime = document.createElement('label');
+        postTime.className = 'wall-post-item-post-time';
+        postTime.textContent = item.relative_time;
+        header.appendChild(posterName);
+        header.appendChild(postTime);
+        result.appendChild(header);
+        const contents = document.createElement('div');
+        contents.className = 'wall-post-item-contents';
+        contents.innerHTML = item.contents_html;
+        result.appendChild(contents);
         return result;
     }
 }
@@ -100,7 +116,39 @@ class PackagesPanel extends Panel<Package> {
     createListItem(item: Package): HTMLLIElement {
         const result = document.createElement('li');
         result.className = 'package-item';
+        const fields = document.createElement('div');
+        fields.className = 'package-item-fields';
+        fields.appendChild(this.createField('Type', item.type));
+        fields.appendChild(this.createField('Arrival', item.arrival));
+        if (item.description) {
+            fields.appendChild(this.createField('Description', item.description));
+        }
+        fields.appendChild(this.createField('Accepted By', item.accepted_by));
+        fields.appendChild(this.createField('Released By', item.released_by));
+        result.appendChild(fields);
+        const icon = document.createElement('img');
+        icon.className = 'package-item-icon';
+        if (item.type == 'USPS') {
+            icon.src = "/svg/usps.svg";
+        } else {
+            icon.src = "/svg/amazon_box.svg";
+        }
+        result.appendChild(icon);
         return result;
+    }
+
+    createField(name: string, value: string): HTMLElement {
+        const field = document.createElement('div');
+        field.className = 'package-item-field';
+        const nameLabel = document.createElement('label');
+        nameLabel.className = 'package-item-field-name';
+        nameLabel.textContent = name;
+        const valueLabel = document.createElement('label');
+        valueLabel.className = 'package-item-field-value';
+        valueLabel.textContent = value;
+        field.appendChild(nameLabel);
+        field.appendChild(valueLabel);
+        return field;
     }
 }
 
